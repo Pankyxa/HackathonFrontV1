671b4802e41372cc32e18c8daf4eef9ffe71634a
Pankyxa <kbelozerov1@gmail.com>
2025-02-28 15:39:54 +0500

    Stages

--- src/views/JudgePage.vue
+++ src/views/JudgePage.vue
@@ -1,25 +1,45 @@
 <template>
   <TheHeader/>
   <div class="judge-page">
-    <el-tabs v-model="activeTab" class="tabs-container">
-      <el-tab-pane label="Неоцененные работы" name="unevaluated">
-        <UnevaluatedTeamsTab/>
-      </el-tab-pane>
+    <div v-if="canJudge">
+      <el-tabs v-model="activeTab" class="tabs-container">
+        <el-tab-pane label="Неоцененные работы" name="unevaluated">
+          <UnevaluatedTeamsTab/>
+        </el-tab-pane>
 
-      <el-tab-pane label="Оцененные работы" name="evaluated">
-        <EvaluatedTeamsTab/>
-      </el-tab-pane>
-    </el-tabs>
+        <el-tab-pane label="Оцененные работы" name="evaluated">
+          <EvaluatedTeamsTab/>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+    <div v-else class="message-container">
+      <el-alert
+          title="Оценка работ пока недоступна"
+          type="info"
+          :closable="false"
+          description="Вы сможете приступить к оценке работ после начала этапа проверки решений."
+          show-icon
+      />
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import TheHeader from "@/components/TheHeader.vue"
 import UnevaluatedTeamsTab from "@/components/judge/UnevaluatedTeamsTab.vue"
 import EvaluatedTeamsTab from "@/components/judge/EvaluatedTeamsTab.vue"
+import { useStageStore } from "@/stores/stage.js";
 
+const stageStore = useStageStore();
 const activeTab = ref('unevaluated')
+
+const canJudge = computed(() => {
+  return stageStore.isSolutionReview ||
+      stageStore.isOnlineDefense ||
+      stageStore.isResultsPublication ||
+      stageStore.isAwardCeremony;
+})
 </script>
 
 <style scoped>
@@ -40,6 +60,14 @@ const activeTab = ref('unevaluated')
   display: flex;
 }
 
+.message-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  padding: 20px;
+}
+
 :deep(.el-tabs) {
   display: flex;
   height: 100%;
@@ -53,6 +81,12 @@ const activeTab = ref('unevaluated')
 :deep(.el-tab-pane) {
   height: 100%;
 }
+
+:deep(.el-alert) {
+  width: 100%;
+  max-width: 600px;
+}
+
 @media (max-width: 768px) {
   .judge-page {
     height: auto;
